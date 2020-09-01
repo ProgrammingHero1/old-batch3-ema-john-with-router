417bc381a5963d49a36f7bda37c5c232aa519234
Mujahid-Developer <mujahidulislam575@gmail.com>
2020-09-01 23:41:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
